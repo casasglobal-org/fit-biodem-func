e02df33877339eeee6ca19e9d76dbb347b085f09
Luigi Ponti <quartese@gmail.com>
2021-03-23 19:49:20 +0100

    Fixed poetry init of existing project

---
+++ fit_lib.py
@@ -0,0 +1,9 @@
+"""Library of statisitical algorithms to fit biodemographic functions
+included in module bdm_lib.py for modeling basic biological processes in any
+organism using physiologically based demographic models (PBDMs, see
+https://doi.org/10.1111/epp.12224).
+"""
+
+from bdm_lib import development_rate
+
+
