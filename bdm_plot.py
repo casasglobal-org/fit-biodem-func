f1a627395122c72bafae1bad62f63364da65c9f6
Luigi Ponti <quartese@gmail.com>
2021-03-24 16:04:11 +0100

    Initialized plotting feature.

---
+++ bdm_plot.py
@@ -0,0 +1,7 @@
+"""Get plotting code from bdm_lib.py, implement it here and import functions
+here for modeling basic biological processes in any organism using
+physiologically based demographic models (PBDMs, see https
+"""
+
+from bdm_lib import development_rate
+
