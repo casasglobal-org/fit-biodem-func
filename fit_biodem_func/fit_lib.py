b0669175eeea702251f3e5d36b38159270b48b04
Luigi Ponti <quartese@gmail.com>
2021-04-01 13:35:28 +0200

    Started to write some lmfit code but from lmfit import Model, Parameter, report_fit fails with ModuleNotFoundError: No module named 'lmfit', which works form cli.

--- fit_biodem_func/fit_lib.py
+++ fit_biodem_func/fit_lib.py
@@ -4,21 +4,30 @@ organism using physiologically based demographic models (PBDMs, see
 https://doi.org/10.1111/epp.12224).
 """
 
-import lmfit
+import numpy as np
 
+from lmfit import Model, Parameter, report_fit
 from bdm_lib import development_rate
 
+# Data:
+
+# Experimental temperatures
+temperature_list = [10, 15, 20, 25, 30, 17, 22, 26, 28, 30, 33, 14, 19, 27, 6,
+                    10, 15, 20, 25, 30, 33, 36]
+# Developmental rates measured at the experimental temperatures above
+development_rate_list_tuta = [0.0087, 0.0156, 0.0286, 0.0435, 0.0556, 0.0154,
+                              0.0312, 0.0417, 0.0500, 0.0500, 0.0588, 0.0131,
+                              0.0250, 0.0417, 0.0080, 0.0099, 0.0173, 0.0276,
+                              0.0409, 0.0508, 0.0546, 0]
+
+# Model:
+model = Model(development_rate, independent_vars=['temperature'])
+result = model.fit(development_rate_list_tuta,
+                   temperature=temperature_list)
+
+print(result.values)
 
 # Check out
 # https://lmfit.github.io/lmfit-py/
 # https://zenodo.org/badge/DOI/10.5281/zenodo.4516651.svg
 # https://mike.depalatis.net/blog/lmfit.html
-
-f = Parameter(name (str) – Name of the Parameter,
-    value (float, optional) – Numerical Parameter value.
-    vary (bool, optional) – Whether the Parameter is varied during a fit (default is True).
-    min (float, optional) – Lower bound for value (default is -numpy.inf, no lower bound).
-    max (float, optional) – Upper bound for value (default is numpy.inf, no upper bound).
-    expr (str, optional) – Mathematical expression used to constrain the value during the fit (default is None).
-    brute_step (float, optional) – Step size for grid points in the brute method (default is None).
-    user_data (optional) – User-definable extra attribute used for a Parameter (default is None).)
