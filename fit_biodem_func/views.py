49700df8d5ace39e42fc745cf0d23c9d76606bd4
Luigi Ponti <quartese@gmail.com>
2021-04-27 13:05:38 +0200

    trying to load and read csv on aws

--- fit_biodem_func/views.py
+++ fit_biodem_func/views.py
@@ -1,20 +1,42 @@
 import os
+import boto3
 from flask import (flash, request, render_template,
                    redirect, send_from_directory)
 from werkzeug.utils import secure_filename
 
 from .user_data import create_app
-from .utils import fit_uploaded_data, create_plot
+from .utils import fit_uploaded_data_aws, create_plot
 
-UPLOAD_FOLDER = os.path.join(os.getcwd(), 'fit_biodem_func/uploads')
-PLOT_FOLDER = os.path.join(os.getcwd(), 'fit_biodem_func/plots')
+# UPLOAD_FOLDER = os.path.join(os.getcwd(), 'fit_biodem_func/uploads')
+# PLOT_FOLDER = os.path.join(os.getcwd(), 'fit_biodem_func/plots')
 ALLOWED_EXTENSIONS = {'txt', 'csv'}
 
 app = create_app()
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['PLOT_FOLDER'] = PLOT_FOLDER
+# The old local way
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['PLOT_FOLDER'] = PLOT_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
+
+
+# The AWS way
+FILE_URL = 'https://{bucket}.s3.eu-west-1.amazonaws.com/{filename}'
+S3_BUCKET = 'fit-biodem-func'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+
+def upload_to_s3(filepath, bucket=S3_BUCKET):
+    session = boto3.Session(
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+    )
+    s3 = session.resource('s3')
+    ret = s3.Bucket(bucket).put_object(
+        Key=os.path.basename(filepath),
+        Body=open(filepath, 'rb'),
+        ACL='public-read')
+    return FILE_URL.format(bucket=bucket, filename=ret.key)
 
 
 def allowed_file(filename):
@@ -42,15 +64,23 @@ def upload_file():
             return redirect(request.url)
 
         if not allowed_file(file.filename):
-            flash(f'File extension not allowed ({", ".join(ALLOWED_EXTENSIONS)})')
+            msg = (
+                    f'File extension not allowed'
+                    f' ({", ".join(ALLOWED_EXTENSIONS)})'
+            )
+            flash(msg)
             return redirect(request.url)
 
         filename = secure_filename(file.filename)
 
-        csv_upload_file = os.path.join(
-            app.config['UPLOAD_FOLDER'], filename)
-        file.save(csv_upload_file)
-        fit = fit_uploaded_data(csv_upload_file)
+        # csv_upload_file = os.path.join(
+        #     app.config['UPLOAD_FOLDER'], filename)
+        # file.save(csv_upload_file)
+
+        csv_upload_file = 'uploads/' + filename
+        upload_to_s3(csv_upload_file)
+
+        fit = fit_uploaded_data_aws(csv_upload_file, S3_BUCKET)
         fit_report_string = fit.fit_report().splitlines()
 
         png_plot_file = create_plot(app.config['PLOT_FOLDER'], fit, filename)
